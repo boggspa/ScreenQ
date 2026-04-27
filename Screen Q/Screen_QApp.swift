ac8d9eb8b3f9a2468439525f843d4c4a325124ee
Chris Izatt <chrisizatt@hotmail.co.uk>
2026-04-27 06:36:12 +0100

    Initial Commit

---
+++ Screen Q/Screen_QApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_QApp.swift
+//  Screen Q
+//
+//  Created by Chris Izatt on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Screen_QApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
